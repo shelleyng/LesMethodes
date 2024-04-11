bab07f8af27b0ea9ab93c1b7d1f999cc93c59e86
shelleyng <shelleyn208@gmail.com>
2024-04-11 13:43:37 -0400

    fourth

--- src/exerciseformative4.java
+++ src/exerciseformative4.java
@@ -3,19 +3,32 @@ public class exerciseformative4
     public static void main(String[] args)
     {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println("The sum is " + sum(matrix));
+        System.out.println("The max is " + max(matrix));
+    }
+    public static int sum(int[][] matrix) {
         int sum = 0;
-        int max = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 sum += matrix[i][j];
+
+            }
+        }
+        return sum;
+    }
+    public static int max(int[][] matrix) {
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] > max)
                 {
                     max = matrix[i][j];
                 }
             }
         }
-        System.out.println("The sum is " +sum);
-        System.out.println("The max is " +max);
+        return max;
     }
+
 
 }
--- src/exo1formatif.java
+++ src/exo1formatif.java
@@ -4,13 +4,23 @@ public class exo1formatif
     public static void main(String[] args)
     {
         int[] array = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("The sum is " + sum(array));
+
+        System.out.println("The maximum is " + max(array));
+
+    }
+    public static int sum(int[] array)
+    {
         int sum = 0;
         for (int i = 0; i < array.length; i++)
         {
             sum += array[i];
         }
-        System.out.println("The sum is " + sum);
-
+        return sum;
+    }
+    public static int max(int[] array)
+    {
         int max = 0;
         for (int i = 0; i < array.length; i++)
         {
@@ -18,8 +28,7 @@ public class exo1formatif
                 max = array[i];
             }
         }
-        System.out.println("The maximum is " + max);
-
+        return max;
     }
 
 }
